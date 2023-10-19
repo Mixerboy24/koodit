a4c0c56610ee5215d25122829214643e59577b63
Atte Oksanen <mixerboy24@gmail.com>
2023-10-19 04:33:43 +0300

    Update docker-debian.sh
    Add docker-compose

--- Linux-sh/Docker/docker-debian.sh
+++ Linux-sh/Docker/docker-debian.sh
@@ -14,6 +14,6 @@ echo \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update -y
 
-sudo apt-get -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt-get -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin docker-compose
 
 echo "Docker Installed!"
